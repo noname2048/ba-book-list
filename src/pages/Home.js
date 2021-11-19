390f9787cbb50b32fc7f52a977b9dc558fa05569
noname2048 <sungwook.csw@gmail.com>
2021-11-19 22:57:32 +0900

    chore: add some template component

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <h1>WELCOME HOME</h1>
+        );
+    }
+}
+
+export default Home;
