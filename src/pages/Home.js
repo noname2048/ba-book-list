d4ea6bc3eb5b1182ac110db1d01aa975ca289e40
noname2048 <sungwook.csw@gmail.com>
2021-11-23 23:27:45 +0900

    chore: reduce tabsize

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,21 @@
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
-import IsbnSearch from "../components/IsbnSearch"
+import IsbnSearch from "../components/IsbnSearch";
+import { Main } from "../components/Converters";
 import React from "react";
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>WELCOME HOME</h1>
-                <Routes>
-                    <Route path="#" element={<IsbnSearch />}/>
-                </Routes>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>WELCOME HOME</h1>
+        <Routes>
+          <Route path="#" element={<IsbnSearch />} />
+        </Routes>
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default Home;
