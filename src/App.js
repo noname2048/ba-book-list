57a001ba1473f658270952b1889bf96db08b70eb
noname2048 <sungwook.csw@gmail.com>
2021-12-12 23:11:20 +0900

    fix: fix app to render

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import "./App.css";
 
-import TodoApp from "./components/TodoApp";
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+// import mvp from "../public/css/mvp.module.css";
+// import "../public/css/mvp.css";
+import "./mvp.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <TodoApp></TodoApp>
-      <h1 style={styles.title}>Welcom back!</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
