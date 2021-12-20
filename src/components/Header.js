0b3b3b95e133c30951cd951cd9e415b5ff9d77fd
noname2048 <sungwook.csw@gmail.com>
2021-12-20 23:39:06 +0900

    design: add some design in header, input center not work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,59 @@
 // import { useState } from "react";
 import { Link } from "react-router-dom";
 import { GoSearch } from "react-icons/go";
+import styled from "styled-components";
+
+const TitleH1 = styled.h1`
+  margin: 1.2rem;
+`;
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SearchInputDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #181818;
+  border-radius: 1.2rem;
+`;
+
+const StyledInput = styled.input`
+  display: block;
+`;
+
+const LoginStatusDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 1.2rem;
+`;
+
+const StyledLink = styled(Link)`
+  padding: 0.5rem;
+  margin: 0.5rem;
+`;
 
 const Header = () => {
   return (
-    <div>
-      <h1>서지검색</h1>
-      <div style={{ display: "flex" }}>
+    <MainDiv>
+      <TitleH1>서지검색</TitleH1>
+      <SearchInputDiv>
         <GoSearch />
+        <span style={{ width: "100px", display: "block" }}>
+          <StyledInput type="text" />
+        </span>
         <span>검색하기</span>
-      </div>
-      <div>
-        <Link to="/login">로그인</Link>
-        <Link to="/signup">회원가입</Link>
-      </div>
-    </div>
+      </SearchInputDiv>
+      <LoginStatusDiv>
+        <StyledLink to="/login">로그인</StyledLink>
+        <StyledLink to="/signup">회원가입</StyledLink>
+      </LoginStatusDiv>
+    </MainDiv>
   );
 };
 
