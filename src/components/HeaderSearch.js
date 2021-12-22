b1f9a567ad1fe4d2ea4cb6e8244e6332f4e9b408
noname2048 <sungwook.csw@gmail.com>
2021-12-22 23:59:23 +0900

    feat: add black and red text

--- src/components/HeaderSearch.js
+++ src/components/HeaderSearch.js
@@ -1,25 +1,38 @@
+import { executeReducerBuilderCallback } from "@reduxjs/toolkit/dist/mapBuilders";
 import { useState } from "react";
 import styled from "styled-components";
 const HeaderSearch = () => {
   const [searchText, setSearchText] = useState("");
   let value = "";
 
+  const isbnRegx = /[0-9-]/gi;
+
   return (
-    <input
-      type="text"
-      placeholder="ISBN 검색하기"
-      value={searchText}
-      onChange={(e) => {
-        setSearchText(e.target.value);
-      }}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          value = e.target.value;
-          console.log(`search ${value}`);
-          setSearchText("");
-        }
-      }}
-    />
+    <div>
+      <input
+        type="text"
+        placeholder="ISBN 검색하기"
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            value = e.target.value;
+            console.log(`search ${value}`);
+            setSearchText("");
+          } else if (!e.key.match(isbnRegx)) {
+            console.log("wrong input");
+          }
+        }}
+      />
+      {searchText.split().map((t, idx) => (
+        <span
+          key={idx}
+          style={{ color: t.match(isbnRegx) ? "black" : "red" }}
+        ></span>
+      ))}
+    </div>
   );
 };
 
