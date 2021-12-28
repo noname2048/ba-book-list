460ddaa0cced1454c49504a388eb43d73678d1a1
noname2048 <sungwook.csw@gmail.com>
2021-12-29 00:42:22 +0900

    feat: update bookSearchResult

--- src/pages/BookSearchResult.js
+++ src/pages/BookSearchResult.js
@@ -4,39 +4,39 @@ import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 
 export default function BookSearchResult() {
-  const [result, setResult] = useState([]);
   const location = useLocation();
   const { title, isbn13 } = queryString.parse(location.search);
-
-  const fetchData = async () => {
-    const url = `http://localhost:8000/books${
-      title ? `title=${title}` : "" + isbn13 ? `isbn13=${isbn13}` : ""
-    }`;
-
-    try {
-      const response = await fetch(url);
-      const jsonData = await response.json();
-      setResult(jsonData.books);
-    } catch (err) {
-      setResult({ error: "error ocurs" });
-    }
-  };
+  const [searchResult, setSearchResult] = useState([]);
 
   useEffect(() => {
+    async function fetchData() {
+      const qs = [
+        title ? `title=${title}` : "",
+        isbn13 ? `isbn13=${isbn13}` : "",
+      ].join("");
+
+      let url = "http://localhost:8000/api/v1/books";
+      if (qs !== "") {
+        url = url + "?" + qs;
+      }
+
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setSearchResult(jsonData.books);
+        console.log(jsonData);
+      } catch (err) {
+        setSearchResult([]);
+      }
+    }
     fetchData();
   }, [title, isbn13]);
 
   return (
     <>
-      {isbn13 || title ? (
-        <div>
-          {result.map(() => (
-            <p>{result}</p>
-          ))}
-        </div>
-      ) : (
-        <p>isbn13연결바람</p>
-      )}
+      {searchResult
+        ? searchResult.map((item, idx) => item)
+        : "검색결과가 존재하지 않습니다"}
     </>
   );
 }
